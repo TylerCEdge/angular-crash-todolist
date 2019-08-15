c6b71ff57d778d4fe7aebf38a727f2008ac8e542
TylerCEdge <tylercedge@gmail.com>
2019-08-15 18:37:21 -0400

    delete functionality now working via new delete request made for jsonplaceholder module

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { TodoService } from '../../services/todo.service'
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -9,6 +9,7 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
   constructor(private todoService: TodoService) { }
 
@@ -17,7 +18,7 @@ export class TodoItemComponent implements OnInit {
 
   // Set Dynamic Classes
   setClasses() {
-    const classes = {
+    let classes = {
       todo: true,
       'is-complete': this.todo.completed
     };
@@ -26,11 +27,14 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
-    console.log('delete');
+    this.deleteTodo.emit(todo);
   }
 
 }

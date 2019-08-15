b85a7a955de80a0181a8205e07fa8b8d030ec5da
TylerCEdge <tylercedge@gmail.com>
2019-08-15 18:11:21 -0400

    jsonplaceholder module now being used to generate todos.  Using template literals to limit get requests to 5 items.

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -14,7 +14,9 @@ export class TodosComponent implements OnInit {
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-    this.todos = this.todoService.getTodos();
+    this.todos = this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
 }
